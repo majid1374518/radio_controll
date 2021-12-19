be3e79b146966fa9fcf214694421fdfa6436e025
sajjad gharibi <79171301+sajjadgharibi@users.noreply.github.com>
2021-12-19 13:41:48 +0330

    get back to old code

--- transmitter/src/radio_control_transmitter.ino
+++ transmitter/src/radio_control_transmitter.ino
@@ -15,7 +15,7 @@ void loop() {
   int Yaw = analogRead(A2);
   int Esd = digitalRead(Emergency_shut_down_pin);
   int PID_tuner =  analogRead(A1);
-  //  String p1 = "\t";
-  //  Serial.println(Trottle + p1 + Pitch + p1 + Roll + p1 + Yaw + p1 + Esd + p1 + PID_tuner);
-  Serial.print(Trottle +'T' + Pitch + 'P' + Roll + 'R' + Yaw + 'Y' + Esd + 'E' + PID_tuner +  'D');
+    String p1 = "\t";
+  Serial.println(Trottle + p1 + Pitch + p1 + Roll + p1 + Yaw + p1 + Esd + p1 + PID_tuner);
+  //Serial.print(Trottle +'T' + Pitch + 'P' + Roll + 'R' + Yaw + 'Y' + Esd + 'E' + PID_tuner +  'D');
 }
