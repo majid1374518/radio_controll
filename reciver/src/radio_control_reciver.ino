1974ac30e368f858c70438af3d89740c766aacbc
majid1374518 <lotfimajid88@yahoo.com>
2021-12-25 22:20:03 +0330

    pwm is made by servo.h library and ppm is made by my function that works fine but this device worked fine until 72 minetus because of micros overflow and return to zero after that time

--- reciver/src/radio_control_reciver.ino
+++ reciver/src/radio_control_reciver.ino
@@ -1,8 +1,8 @@
 /*
   String to Integer conversion
 
-  Reads a serial input string until it sees a newline, then converts the string
-  to a number if the characters are digits.
+  Reads a serial input sthing until it sees a newline, then converts the string
+  to a number if the charhiters are digits.
 
   The circuit:
   - No external components needed.
@@ -14,21 +14,32 @@
 
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/StringToInt
 */
+#define pwm_pin_1 2
+#define pwm_pin_2 3
+#define pwm_pin_3 4
+#define pwm_pin_4 5
+#define pwm_pin_5 6
+#define ESD_pin 9
+#define ppm_pin 8
+#define ppm_high_time 100
+
 #include "Arduino.h"
 #include "math.h"
 #include "Servo.h"
 
+unsigned long int time = 0;
 String inString = ""; // string to hold input
 Servo pwm_channel_1, pwm_channel_2, pwm_channel_3, pwm_channel_4, pwm_channel_5, pwm_channel_6;
+int last_pwm_pin_1_status = 0, last_pwm_pin_2_status = 0, last_pwm_pin_3_status = 0, last_pwm_pin_4_status = 0, last_pwm_pin_5_status = 0;
 void setup()
 {
-  pwm_channel_1.attach(2);
-  pwm_channel_2.attach(3);
-  pwm_channel_3.attach(4);
-  pwm_channel_4.attach(5);
-  pwm_channel_5.attach(6);
-  pwm_channel_6.attach(7);
-  pinMode(8, 1);
+  pwm_channel_1.attach(pwm_pin_1);
+  pwm_channel_2.attach(pwm_pin_2);
+  pwm_channel_3.attach(pwm_pin_3);
+  pwm_channel_4.attach(pwm_pin_4);
+  pwm_channel_5.attach(pwm_pin_5);
+  pinMode(ESD_pin, 1);
+  pinMode(ppm_pin, 1);
   Serial.begin(57600);
   while (!Serial)
   {
@@ -42,6 +53,7 @@ void setup()
 
 void loop()
 {
+  ppm_maker();
   // Read serial input:
   while (Serial.available() > 0)
   {
@@ -53,6 +65,74 @@ void loop()
     }
     check_inchar(inChar);
   }
+}
+void ppm_maker(void)
+{
+  int initial_time = micros();
+  time = micros();
+  int pwm_pin_1_status = digitalRead(pwm_pin_1);
+  if (last_pwm_pin_1_status != pwm_pin_1_status)
+  {
+    digitalWrite(ppm_pin, HIGH);
+    while (micros() - time < ppm_high_time)
+      ;
+    digitalWrite(ppm_pin, LOW);
+    last_pwm_pin_1_status = pwm_pin_1_status;
+  }
+  time = micros();
+  int pwm_pin_2_status = digitalRead(pwm_pin_2);
+  if (last_pwm_pin_2_status != pwm_pin_2_status)
+  {
+    if (pwm_pin_2_status == 0)
+    {
+      digitalWrite(ppm_pin, HIGH);
+      while (micros() - time < ppm_high_time)
+        ;
+      digitalWrite(ppm_pin, LOW);
+    }
+    last_pwm_pin_2_status = pwm_pin_2_status;
+  }
+  time = micros();
+  int pwm_pin_3_status = digitalRead(pwm_pin_3);
+  if (last_pwm_pin_3_status != pwm_pin_3_status)
+  {
+    if (pwm_pin_3_status == 0)
+    {
+      digitalWrite(ppm_pin, HIGH);
+      while (micros() - time < ppm_high_time)
+        ;
+      digitalWrite(ppm_pin, LOW);
+    }
+    last_pwm_pin_3_status = pwm_pin_3_status;
+  }
+  time = micros();
+  int pwm_pin_4_status = digitalRead(pwm_pin_4);
+  if (last_pwm_pin_4_status != pwm_pin_4_status)
+  {
+    if (pwm_pin_4_status == 0)
+    {
+      digitalWrite(ppm_pin, HIGH);
+      while (micros() - time < ppm_high_time)
+        ;
+      digitalWrite(ppm_pin, LOW);
+    }
+    last_pwm_pin_4_status = pwm_pin_4_status;
+  }
+  time = micros();
+  int pwm_pin_5_status = digitalRead(pwm_pin_5);
+  if (last_pwm_pin_5_status != pwm_pin_5_status)
+  {
+    if (pwm_pin_5_status == 0)
+    {
+      digitalWrite(ppm_pin, HIGH);
+      while (micros() - time < ppm_high_time)
+        ;
+      digitalWrite(ppm_pin, LOW);
+    }
+    last_pwm_pin_5_status = pwm_pin_5_status;
+  }
+  while (micros() - initial_time < 25000)
+    ;
 }
 void check_inchar(int inChar)
 {
@@ -101,7 +181,7 @@ void check_inchar(int inChar)
     // Serial.println();
     // Serial.print("ESD=");
     // Serial.println(inString.toInt());
-    digitalWrite(8, inString.toInt());
+    digitalWrite(ESD_pin, inString.toInt());
     inString = "";
   }
   if (inChar == 'D')
